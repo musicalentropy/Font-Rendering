7ff616b5533cbc989ef4ac745941b859c369db19
Ivan Cohen <ivan.cohen@orosys.fr>
2015-05-12 19:06:39 +0200

    Adding all the files

---
+++ JuceLibraryCode/modules/juce_core/juce_core.h
@@ -0,0 +1,11 @@
+// This is an auto-generated file to redirect any included
+// module headers to the correct external folder.
+
+#if defined (JUCER_VS2008_78A5006)
+ #include "C:/SDK/juce-3.1.1/modules/juce_core/juce_core.h"
+#elif defined (JUCER_XCODE_MAC_F6D2F4CF)
+ #include "/Library/SDK/juce-3.1.1/modules/juce_core/juce_core.h"
+#else
+ #error "This file is designed to be used in an Introjucer-generated project!"
+#endif
+
